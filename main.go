6660a38102956bf4f29103a4b4607a9ffac30c4c
Asutosh Panda <78539272+measutosh@users.noreply.github.com>
2023-01-24 18:11:33 +0000

    added get and set command

--- main.go
+++ main.go
@@ -1,10 +1,68 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+func handleConnection(db map[string]string, client net.Conn) {
+  // recieves the message sent by the client using bufio
+  // runs the goroutine infinitely
+
+  defer client.Close()
+
+  fmt.Printf("Serving %s\n", client.RemoteAddr().String())
+
+  for{
+    netData, err := bufio.NewReader(client).ReadString('\n')
+    if err != nil {
+      fmt.Println("error reading: ", err)
+      return
+    }
+
+    var response string
+    commandString := strings.TrimSpace(netData)
+    parts := strings.Split(commandString, " ")
+    command := parts[0]
+
+    switch command {
+      case "STOP", "QUIT":
+        return
+      case "GET":
+        if len(parts) > 1 {
+        	key := parts[1]
+        
+        	response = db[key]
+        } else {
+        	response = "ERR wrong number of arguments for 'get'"
+        }
+      case "SET":
+        if len(parts) > 2 {
+        	key := parts[1]
+        	value := parts[2]
+        
+        	db[key] = value
+        	response = "OK"
+        } else {
+        	response = "ERR wrong number of arguments for 'set' command"
+        }
+      case "DEL":
+      case "INCR":
+        // if values exists then convert the text into int
+        // increase the string
+        // convert it back to string and store in the map 
+      default:
+        response = "ERR unknown command"
+    }
+    client.Write([]byte(response + "\n"))
+  }
+
+  fmt.Println("Closing the client")
+  client.Close()
+}
 
 func main() {
 	arguments := os.Args
@@ -17,12 +75,15 @@ func main() {
   server, err := net.Listen("tcp4", port)
   if err != nil {
     fmt.Println(err)
+    return 
   }
   defer server.Close()
 
   // main goroutine runs infinitely
   // when it gets a request from a client, 
   // it runs that request in a separate goroutine
+  // map acts as database and is passed to all goroutines
+  db := make(map[string]string)
 
   for{
     client, err := server.Accept()
@@ -31,7 +92,7 @@ func main() {
       return
     }
 
-    go handleConnection(client)
+    go handleConnection(db, client)
   }
 }
 
