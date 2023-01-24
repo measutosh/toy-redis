07be3e6dfb636f6089506d2c60f7938f0627b25e
Asutosh Panda <78539272+measutosh@users.noreply.github.com>
2023-01-24 17:27:19 +0000

    basic tcp server

---
+++ README.md
@@ -0,0 +1,7 @@
+## A toy redis server, written in Golang.
+
+It supports few the features of Redis
+- reachable over TCP.
+- can handle concurrent requests.
+- GET, SET, DEL, INCR methods are implemented.
+- uses only standard libraries.
--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number")
+		return
+	}
+
+  port := ":" + arguments[1]
+  server, err := net.Listen("tcp4", port)
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer server.Close()
+
+  // main goroutine runs infinitely
+  // when it gets a request from a client, 
+  // it runs that request in a separate goroutine
+
+  for{
+    client, err := server.Accept()
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+
+    go handleConnection(client)
+  }
 }
+
+
+
+
+
+
+
+
+
+
