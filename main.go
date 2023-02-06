97b84fe944490b898df8410de7e09fae867d95a7
Asutosh Panda <78539272+measutosh@users.noreply.github.com>
2023-02-06 17:38:20 +0000

    modified the code to avoid race conditions, code completed

--- main.go
+++ main.go
@@ -9,92 +9,150 @@ import (
 	"strings"
 )
 
-func handleConnection(db map[string]string, client net.Conn) {
-  // recieves the message sent by the client using bufio
-  // runs the goroutine infinitely
+type Command int
 
-  defer client.Close()
+const (
+	Get Command = iota + 1
+	Set
+	Incr
+	Del
+)
 
-  fmt.Printf("Serving %s\n", client.RemoteAddr().String())
+type commandMessage struct {
+	commandName     Command
+	key             string
+	value           string
+	responseChannel chan string
+}
 
-  for{
-    netData, err := bufio.NewReader(client).ReadString('\n')
-    if err != nil {
-      fmt.Println("error reading: ", err)
-      return
+func handleDB(commandChannel chan CommandMessage) {
+    db := make(map[string]string)
+
+    for {
+        select {
+            case command := <- commandChannel:
+                switch command.commandName {
+                    case Get:
+                        command.responseChannel <- db[command.key]
+                    case Set:
+                        db[command.key] = command.value
+                        command.responseChannel <- "OK"
+                    case Incr:
+                        value, ok := db[command, key]
+                        var response string
+
+                        if ok {
+                            intValue, err := strconv.Atoi(value)
+                            if err != nil {
+                                response = "ERR value is noy an integer or out of range"
+                            } else {
+                                response = strconv.Itoa(intValue + 1)
+                                db[command.key] = response
+                            }
+                        } else {
+                            response = "1"
+                            db[command.key] = response
+                        }
+    
+                        command.responseChannel <- response
+                    case Del:
+                        _, ok := db[command.key]
+                    var response string
+
+                    if ok {
+                        delete(db, command.key)
+                        response = "1"
+                    } else {
+                        response = "0"
+                    }
+                    command.responseChannel <- response
+                }
+        }
     }
+}
 
-    var response string
-    commandString := strings.TrimSpace(netData)
-    parts := strings.Split(commandString, " ")
-    command := parts[0]
+func handleConnection(commandChannel chan commandMessage, client net.Conn) {
 
-    switch command {
-      case "STOP", "QUIT":
-        return
-      case "GET":
-        if len(parts) > 1 {
-        	key := parts[1]
-        
-        	response = db[key]
-        } else {
-        	response = "ERR wrong number of arguments for 'get'"
-        }
-      case "SET":
-        if len(parts) > 2 {
-        	key := parts[1]
-        	value := parts[2]
-        
-        	db[key] = value
-        	response = "OK"
-        } else {
-        	response = "ERR wrong number of arguments for 'set' command"
-        }
-      case "DEL":
-        if len(parts) > 1 {
-          key := parts[1]
-          _, ok := db[key]
+	defer client.Close()
 
-          if ok {
-            delete(db, key)
-            response = "1"
-          } else {
-            response = "0"
-          }
-        } else {
-          response = "ERR wrong number of arguments for 'del'"
-        }
-      case "INCR":
-        // if values exists then convert the text into int
-        // increase the string
-        // convert it back to string and store in the map
-        if len(parts) > 1 {
-        	key := parts[1]
-        	value, ok := db[key]
-        
-        	if ok {
-        		intValue, err := strconv.Atoi(value)
-        		if err != nil {
-        			response = "ERR value is not an integer or out of range"
-        		} else {
-        			response = strconv.Itoa(intValue + 1)
-        			db[key] = response
-        		}
-        	} else {
-        		response = "1"
-        		db[key] = response
-        	}
-        } else {
-        	response = "ERR wrong number of arguments for 'incr' command"
-        }
-      default:
-        response = "ERR unknown command"
-    }
-    client.Write([]byte(response + "\n"))
-  }
+	fmt.Printf("Serving %s\n", client.RemoteAddr().String())
 
-  fmt.Println("Closing the client")
-  client.Close()
+	for {
+		netData, err := bufio.NewReader(client).ReadString('\n')
+		if err != nil {
+			fmt.Println("error reading: ", err)
+			return
+		}
+
+		var response string
+		commandString := strings.TrimSpace(netData)
+		parts := strings.Split(commandString, " ")
+		command := parts[0]
+
+		switch command {
+		case "STOP", "QUIT":
+			return
+		case "GET":
+			if len(parts) > 1 {
+				key := parts[1]
+				commandMessage := commandMessage{
+					commandName:     Get,
+					key:             key,
+					responseChannel: make(chan string),
+				}
+				commandChannel <- commandMessage
+				response = <-commandMessage.responseChannel
+			} else {
+				response = "ERR wrong number of arguments for 'get' command"
+			}
+		case "SET":
+			if len(parts) > 2 {
+				key := parts[1]
+				value := parts[2]
+				commandMessage := commandMessage{
+					commandName:     Set,
+					key:             key,
+					value:           value,
+					responseChannel: make(chan string),
+				}
+				commandChannel <- commandMessage
+				response = <-commandMessage.responseChannel
+
+			} else {
+				response = "ERR wrong number of arguments for 'set' command"
+			}
+		case "DEL":
+			key := parts[1]
+			commandMessage := commandMessage{
+				commandName:     Del,
+				key:             key,
+				responseChannel: make(chan string),
+			}
+
+			commandChannel <- commandMessage
+			response = <-commandMessage.responseChannel
+		case "INCR":
+			// if values exists then convert the text into int
+			// increase the string
+			// convert it back to string and store in the map
+			if len(parts) > 1 {
+				key := parts[1]
+				commandMessage := commandMessage{
+					commandName:     Incr,
+					key:             key,
+					responseChannel: make(chan string),
+				}
+				commandChannel <- commandMessage
+				response = <-commandMessage.responseChannel
+			} else {
+				response = "ERR wrong number of arguments for 'incr' command"
+			}
+		default:
+			response = "ERR unknown command"
+		}
+		client.Write([]byte(response + "\n"))
+	}
+
 }
 
 func main() {
@@ -104,37 +162,25 @@ func main() {
 		return
 	}
 
-  port := ":" + arguments[1]
-  server, err := net.Listen("tcp4", port)
-  if err != nil {
-    fmt.Println(err)
-    return 
-  }
-  defer server.Close()
+	port := ":" + arguments[1]
+	server, err := net.Listen("tcp4", port)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer server.Close()
 
-  // main goroutine runs infinitely
-  // when it gets a request from a client, 
-  // it runs that request in a separate goroutine
-  // map acts as database and is passed to all goroutines
-  db := make(map[string]string)
 
-  for{
-    client, err := server.Accept()
-    if err != nil {
-      fmt.Println(err)
-      return
-    }
+	commandChannel := make(chan commandMessage)
+	go handleDB(commandChannel)
 
-    go handleConnection(db, client)
-  }
+	for {
+		client, err := server.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		go handleConnection(commandChannel, client)
+	}
 }
-
-
-
-
-
-
-
-
-
-
